8e89718fdef9759fa1c034b914c90a7580aa8781
shaman <shaman@shamans-MacBook-Pro.local>
2022-11-20 00:35:02 -0600

    Module 2, Lesson 2

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use stript';
+{
+  const modalHeader = document.querySelectorAll('.modal__title'),
+    modalForm = document.querySelectorAll('.modal__fieldset'),
+    modalCheckbox = document.querySelectorAll('.modal__checkbox'),
+    modalInputDiscount = document.querySelectorAll('.modal__input_discount');
+}
