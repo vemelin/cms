7f489ad88c03f825723f7d650abb8ece724ded59
shaman <shaman@shamans-MacBook-Pro.local>
2022-11-21 08:25:15 -0600

    Main.js replace selectorAll to single index Selector

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use stript';
 {
-  const modalHeader = document.querySelectorAll('.modal__title'),
-    modalForm = document.querySelectorAll('.modal__fieldset'),
-    modalCheckbox = document.querySelectorAll('.modal__checkbox'),
-    modalInputDiscount = document.querySelectorAll('.modal__input_discount');
+  const modalHeader = document.querySelector('.modal__title'),
+    modalForm = document.querySelector('.modal__fieldset'),
+    modalCheckbox = document.querySelector('.modal__checkbox'),
+    modalInputDiscount = document.querySelector('.modal__input_discount');
+    console.log(modalForm);
 }
